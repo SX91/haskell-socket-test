ed48f22c9b949c8b294c0700ec8b5521d046fb1b
Anton K <SX91@protonmail.com>
2017-03-09 12:45:53 +0300

    Test 0.8.0.1 memory consumption
    Signed-off-by: Anton K <SX91@protonmail.com>

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,55 @@
+{-# LANGUAGE BangPatterns      #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import           Control.Concurrent
+import           Control.Concurrent.Async    as Async
 import           Control.Monad
+import           Data.ByteString             (ByteString)
 import           System.Environment
+import           System.Random
 import           System.Socket               as Sock
 import           System.Socket.Family.Inet6  as Inet6
 import           System.Socket.Protocol.UDP  as UDP
 import           System.Socket.Type.Datagram as Dgram
 import           System.Timeout
 
+type UdpSocket inet = Socket inet Dgram.Datagram UDP.UDP
+
+newUdpSocket :: Sock.Family f => Sock.SocketAddress f -> IO (UdpSocket f)
+newUdpSocket bindAddress = do
+    sock <- socket
+    setSocketOption sock (ReuseAddress True)
+    -- setSocketOption sock (Inet6.V6Only False)
+    bind sock bindAddress
+    return sock
+
+serverLoop :: (Sock.Family f) => UdpSocket f -> IO ()
+serverLoop sock = forever $ do
+    (msg, sender) <- receiveFrom sock 65507 mempty
+    let msg = "pong" :: ByteString
+     in sendTo sock msg mempty sender
+
+sendReceiveLoop :: (Sock.Family f) => Sock.SocketAddress f -> UdpSocket f -> IO ()
+sendReceiveLoop serverAddress sock = forever $ do
+    let msg = "ping" :: ByteString
+    sendTo sock msg mempty serverAddress
+    (!response, sender) <- receiveFrom sock 65507 mempty
+    delay <- (*) 1000 <$> randomRIO (1, 300)
+    threadDelay delay
+
 
 main :: IO ()
 main = do
-    -- Get timeout from arguments
-    to <- read . head <$> getArgs
+    -- Get threads num from arguments
+    threads <- read . head <$> getArgs
+
+    let serverAddress = Inet6.SocketAddressInet6 Inet6.inet6Any 5252 0 0
+        anyAddress = Inet6.SocketAddressInet6 Inet6.inet6Any 0 0 0
 
     -- Create, configure and bind socket
-    sock <- socket :: IO (Socket Inet6.Inet6 Dgram.Datagram UDP.UDP)
-    setSocketOption sock (ReuseAddress True)
-    setSocketOption sock (Inet6.V6Only False)
-    bind sock (Inet6.SocketAddressInet6 Inet6.inet6Any 0 0 0)
-
-    -- Now lets start receive-abort-receive cycle
-    forever $
-        timeout to $ receiveFrom sock 65535 mempty
+    server <- newUdpSocket serverAddress
+    withAsync (serverLoop server) $ \_ ->
+        replicateConcurrently threads
+            (newUdpSocket anyAddress >>= sendReceiveLoop serverAddress)
+    return ()
